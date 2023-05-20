277b0672ef5d865abe8114a790f292056ed3f199
vitor-gabriel <gabrijac57@gmail.com>
2023-05-20 19:23:32 -0400

    feat: environment para o angular

--- src/app/sql-editor/services/httpgenerics.service.ts
+++ src/app/sql-editor/services/httpgenerics.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PoTableColumn } from '@po-ui/ng-components';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class HttpGenericsService {
   constructor(private http: HttpClient) { }
 
   getResult(): Observable<{ columns: PoTableColumn[], items: any[] }> {
-    return this.http.get<{ columns: PoTableColumn[], items: any[] }>('http://localhost:3000/resultsql');
+    return this.http.get<{ columns: PoTableColumn[], items: any[] }>(environment.baseURL + 'resultsql');
   }
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  title: 'Desenvolvimento',
+  baseURL: 'http://localhost:3000/'
+}
