b36a613cd76ad358009208e9759a9b2fe9d7f484
vitor-gabriel <gabrijac57@gmail.com>
2023-05-23 16:08:00 -0400

    refactor: auth guarda para nova rota inicial

--- src/app/core/auth-guard.ts
+++ src/app/core/auth-guard.ts
@@ -10,8 +10,9 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
 
-    if (state.url !== '/index.html/login' && !sessionStorage.getItem('PO_USER_LOGIN')) {
-      this.router.navigate(['index.html/login']);
+    console.log(state)
+    if (state.url !== '/index.html' && !sessionStorage.getItem('token')) {
+      this.router.navigate(['index.html']);
       return false;
     }
     return true;
