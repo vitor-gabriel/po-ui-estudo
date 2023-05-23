d836fdc28a6bcc133ed3e12a558d99cccb877d80
vitor-gabriel <gabrijac57@gmail.com>
2023-05-22 20:48:10 -0400

    feat: auth basic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PoPageLoginAuthenticationType } from '@po-ui/ng-templates';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  basic: PoPageLoginAuthenticationType = PoPageLoginAuthenticationType.Basic;
+  loginURL: string = environment.baseURL + 'login';
+  teste: boolean = true;
+
+  constructor() { }
 
 }
