d6fd00698c19d59c4e4dd28979ed41e036f54126
vitor-gabriel <gabrijac57@gmail.com>
2023-05-23 16:08:22 -0400

    feat: conexao com os dois endpoints do protheus

--- src/app/core/services/httpgenerics.service.ts
+++ src/app/core/services/httpgenerics.service.ts
@@ -1,4 +1,5 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Buffer } from 'buffer';
 import { Injectable } from '@angular/core';
 import { PoTableColumn } from '@po-ui/ng-components';
 import { Observable } from 'rxjs';
@@ -8,10 +9,21 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class HttpGenericsService {
+
+  authBasic: string = '';
+
   constructor(private http: HttpClient) { }
 
-  getResult(): Observable<{ columns: PoTableColumn[], items: any[] }> {
-    return this.http.get<{ columns: PoTableColumn[], items: any[] }>(environment.baseURL + 'resultsql');
+  login(formData: any): Observable<any> {
+    this.authBasic = Buffer.from(formData.login + ":" + formData.password, 'ascii').toString('base64');
+    const headers = new HttpHeaders().set('Authorization', 'Basic ' + this.authBasic);
+    return this.http.post<any>(environment.baseURL + '/api/v1/login', { rememberUser: formData.rememberUser }, { headers });
+  }
+
+  getResult(consultText: string): Observable<{ columns: PoTableColumn[], items: any[] }> {
+    const params = new HttpParams().set('query', consultText || '');
+    const headers = new HttpHeaders().set('Authorization', sessionStorage.getItem('token') || '');
+    return this.http.get<{ columns: PoTableColumn[], items: any[] }>(environment.baseURL + '/api/v1/execute', { headers: headers, params: params });
   }
 
 }
